3d139e756795355c9d4d2c304b65ed954330dcf7
gicHan <gichan4444@gmail.com>
2022-06-03 22:55:08 +0900

    DB_first

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var express = require('express'); // 설치한 express module을 불러와서 변수(express)에 담습니다.
 var app = express(); //express를 실행하여 app object를 초기화 합니다.
 
-app.get('/', function(req, res) { // '/' 위치에 'get'요청을 받는 경우,
-  res.send('Hello World!'); // "Hello World!"를 보냅니다.
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/CVS', function(req, res){
+  res.render('CVS');
+});
+
+app.get('/GDS', function(req, res){
+  res.render('GDS');
 });
 
 var port = 3000; // 사용할 포트 번호를 port 변수에 넣습니다.
