7100dc3967b8ff749a3431233f9fac6e41402ffb
dranger97 <dranger97@uos.ac.kr>
2022-06-11 18:44:25 +0900

    connectDB

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 var express = require('express'); // 설치한 express module을 불러와서 변수(express)에 담습니다.
 var app = express(); //express를 실행하여 app object를 초기화 합니다.
 
+var oracledb = require('oracledb');
+var config = {
+    user: "jeongyechan",
+    password: "jeongyechan",
+    connectString: "localhost/xe"
+}
+
+oracledb.getConnection(config, (err, conn) =>{
+    todoWork(err, conn);
+});
+
+function todoWork(err, connection) {
+    if (err) {
+        console.error(err.message);
+        return;
+    }
+    connection.execute("select * from GDS", [], function (err, result) {
+        if (err) {
+            console.error(err.message);
+            doRelease(connection);
+            return;
+        }
+        console.log(result.metaData);  //테이블 스키마
+        console.log(result.rows);  //데이터
+        doRelease(connection);
+    });
+}
+
+function doRelease(connection) {
+    connection.release(function (err) {
+        if (err) {
+            console.error(err.message);
+        }
+    });
+}
+
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
